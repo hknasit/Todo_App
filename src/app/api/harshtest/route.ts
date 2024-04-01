0535aa7b7e77b36687801da458ff2125b4bdf57a
Harsh Nasit <harshnasit1100@gmail.com>
2024-04-01 19:11:52 -0400

    Deleted unwanted folders
    Deleted unwanted folders

--- src/app/api/harshtest/route.ts
+++
@@ -1,7 +0,0 @@
-import {NextRequest, NextResponse} from 'next/server';
-
-export function GET(){
-        return new Promise((resolve, reject) => {
-            return resolve(NextResponse.json({data:"This is testing message"})  )
-        })
-}
